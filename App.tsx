1f07cee76dbdbb010fa14c41215a33980b85bbbf
Jay Bittner <jaybittner@gmail.com>
2021-03-03 19:46:12 -0500

    Added basic navbar

--- App.tsx
+++ App.tsx
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider as PaperProvider, Appbar } from "react-native-paper";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <Appbar style={styles.top}>
+        <Appbar.Action
+          icon="archive"
+          onPress={() => console.log("Pressed archive")}
+        />
+      </Appbar>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
+  top: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 50,
+    zIndex: 900,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
