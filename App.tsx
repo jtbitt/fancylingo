8c1ede0996dc4945609c37aad182830e3aa7f1e8
Jay Bittner <jtbitt@gmail.com>
2021-03-04 16:03:30 -0500

    Set up the basic theme and flash card layout

--- App.tsx
+++ App.tsx
@@ -1,37 +1,97 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Provider as PaperProvider, Appbar } from "react-native-paper";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import {
+  DefaultTheme,
+  Provider as PaperProvider,
+  Appbar,
+  Card,
+  Button,
+  Title,
+} from "react-native-paper";
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#fc5a5e",
+    accent: "#fff0b3",
+  },
+};
 
 export default function App() {
   return (
-    <PaperProvider>
-      <Appbar style={styles.top}>
-        <Appbar.Action
+    <PaperProvider theme={theme}>
+      <Appbar style={styles.nav}>
+        {/* <Appbar.Action
           icon="archive"
           onPress={() => console.log("Pressed archive")}
-        />
+        /> */}
       </Appbar>
       <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
+        <ImageBackground
+          source={require("./assets/background.png")}
+          style={styles.background}
+        >
+          <Card style={styles.card}>
+            <Card.Cover
+              style={styles.image}
+              source={require("./assets/demo-image.png")}
+            />
+            <Card.Content style={styles.content}>
+              <Title style={styles.title}>¿Qué es esto?</Title>
+            </Card.Content>
+          </Card>
+          <Button
+            style={styles.button}
+            mode="contained"
+            onPress={() => console.log("Pressed")}
+          >
+            SEE ANSWER
+          </Button>
+        </ImageBackground>
       </View>
     </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  top: {
+  nav: {
     position: "absolute",
     left: 0,
     right: 0,
     top: 50,
     zIndex: 900,
+    height: 80,
   },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+  },
+  background: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    resizeMode: "cover",
+  },
+  card: {
+    width: "80%",
+  },
+  image: {
+    height: 350,
+  },
+  content: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 50,
+    paddingBottom: 50,
+  },
+  title: {
+    fontSize: 30,
+    color: "black",
+  },
+  button: {
+    width: 150,
+    color: "white",
   },
 });
