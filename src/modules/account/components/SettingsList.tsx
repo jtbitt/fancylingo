ca8f13892585f95ee306f96f908ede30316a00f4
Jay Bittner <jtbitt@gmail.com>
2021-08-03 15:57:04 -0500

    Reorganized settings

---
+++ src/modules/account/components/SettingsList.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Button, List } from "react-native-paper";
+
+interface IDefaultPlayProps {}
+
+export const SettingsList = () => {
+  return (
+    <View style={styles.container}>
+      <List.Item
+        title="Account Settings"
+        style={styles.listItem}
+        titleStyle={styles.listTitle}
+        right={(props) => <List.Icon {...props} icon="chevron-right" />}
+      />
+      <List.Item
+        title="Notification Settings"
+        style={styles.listItem}
+        titleStyle={styles.listTitle}
+        right={(props) => <List.Icon {...props} icon="chevron-right" />}
+      />
+      <List.Item title="" style={styles.listSpacing} />
+      <List.Item
+        title="Privacy Policy"
+        style={styles.listItem}
+        titleStyle={styles.listTitle}
+        right={(props) => <List.Icon {...props} icon="chevron-right" />}
+      />
+      <List.Item
+        title="Q & A"
+        style={styles.listItem}
+        titleStyle={styles.listTitle}
+        right={(props) => <List.Icon {...props} icon="chevron-right" />}
+      />
+      <List.Item
+        title="About Us"
+        style={styles.listItem}
+        titleStyle={styles.listTitle}
+        right={(props) => <List.Icon {...props} icon="chevron-right" />}
+      />
+      <List.Item title="" style={styles.listSpacing} />
+      <List.Item
+        title="Delete Account"
+        style={styles.listItem}
+        titleStyle={[styles.listTitle, styles.delete]}
+        right={(props) => <List.Icon {...props} icon="" />}
+      />
+      <Button
+        mode="contained"
+        style={styles.button}
+        labelStyle={styles.buttonText}
+        onPress={() => console.log("Pressed")}
+      >
+        LOG OUT
+      </Button>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: "5%",
+    justifyContent: "center",
+  },
+  listItem: {
+    height: 35,
+  },
+  listSpacing: {
+    height: 20,
+  },
+  listTitle: {
+    fontSize: 18,
+  },
+  delete: {
+    color: "#A0149E",
+  },
+  button: {
+    alignSelf: "center",
+    marginTop: 80,
+    width: 150,
+    borderRadius: 30,
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+  },
+});
--- src/modules/account/screens/Settings.tsx
+++ src/modules/account/screens/Settings.tsx
@@ -1,61 +1,14 @@
 import React from "react";
 import { StyleSheet, ScrollView, View, Text } from "react-native";
-import { Button, List } from "react-native-paper";
 
 import { UserAvatar } from "../components/UserAvatar";
+import { SettingsList } from "../components/SettingsList";
 
 export const Settings = ({ navigation }: any) => {
   return (
     <View style={styles.container}>
       <UserAvatar />
-      <View style={styles.settingsContainer}>
-        <List.Item
-          title="Account Settings"
-          style={{ height: 35 }}
-          titleStyle={{ fontSize: 18 }}
-          right={(props) => <List.Icon {...props} icon="chevron-right" />}
-        />
-        <List.Item
-          title="Notification Settings"
-          style={{ height: 35 }}
-          titleStyle={{ fontSize: 18 }}
-          right={(props) => <List.Icon {...props} icon="chevron-right" />}
-        />
-        <List.Item title="" style={{ height: 20 }} />
-        <List.Item
-          title="Privacy Policy"
-          style={{ height: 35 }}
-          titleStyle={{ fontSize: 18 }}
-          right={(props) => <List.Icon {...props} icon="chevron-right" />}
-        />
-        <List.Item
-          title="Q & A"
-          style={{ height: 35 }}
-          titleStyle={{ fontSize: 18 }}
-          right={(props) => <List.Icon {...props} icon="chevron-right" />}
-        />
-        <List.Item
-          title="About Us"
-          style={{ height: 35 }}
-          titleStyle={{ fontSize: 18 }}
-          right={(props) => <List.Icon {...props} icon="chevron-right" />}
-        />
-        <List.Item title="" style={{ height: 20 }} />
-        <List.Item
-          title="Delete Account"
-          style={{ height: 35 }}
-          titleStyle={{ fontSize: 18, color: "#A0149E" }}
-          right={(props) => <List.Icon {...props} icon="" />}
-        />
-        <Button
-          mode="contained"
-          style={styles.button}
-          labelStyle={styles.buttonText}
-          onPress={() => console.log("Pressed")}
-        >
-          LOG OUT
-        </Button>
-      </View>
+      <SettingsList />
     </View>
   );
 };
@@ -63,22 +16,5 @@ export const Settings = ({ navigation }: any) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  settingsContainer: {
-    flex: 1,
-    padding: "5%",
-    justifyContent: "center",
-  },
-  button: {
-    alignSelf: "center",
-    marginTop: 80,
-    width: 150,
-    borderRadius: 30,
-    paddingVertical: 10,
-    paddingHorizontal: 5,
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 18,
   },
 });
